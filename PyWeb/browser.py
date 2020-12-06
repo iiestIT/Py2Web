ec2ec48cc6f202d22526eaeda1301dadd1484554
iiestIT <it.iiest.de@gmail.com>
2020-12-06 13:01:20 +0100

    added WebEngineSettings

--- PyWeb/browser.py
+++ PyWeb/browser.py
@@ -1,18 +1,18 @@
-from PySide2.QtCore import QUrl, QTimer
-from PySide2.QtWebEngineWidgets import QWebEngineView, QWebEngineProfile, QWebEnginePage, QWebEngineSettings
+from PySide2.QtCore import QUrl, QTimer, Qt
+from PySide2.QtWebEngineWidgets import QWebEngineView, QWebEngineProfile, QWebEnginePage
+from PySide2.QtWebEngineWidgets import QWebEngineSettings as ws
 from PySide2.QtWebEngineCore import QWebEngineHttpRequest
-from PySide2.QtWidgets import QApplication, QMainWindow
+from PySide2.QtWidgets import QMainWindow, QDialog
 from PySide2.QtNetwork import QNetworkCookie
 from PyWeb.config import BaseConfig as bconf
-import sys
 
 
-class PyWebBrowser(QMainWindow):
+class PyWebBrowser(QDialog):
     def __init__(self):
         super(PyWebBrowser, self).__init__()
-
         self.pwb = QWebEngineView()
-        self._init_settings()
+        self.pwb.setAttribute(Qt.WA_DeleteOnClose, True)
+        self._settings()
 
         self.raw_cookies = []
         self.cookie_list = []
@@ -28,6 +28,22 @@ class PyWebBrowser(QMainWindow):
         wp = QWebEnginePage(profile, self.pwb)
         self.pwb.setPage(wp)
 
+    def _settings(self):
+        s = self.pwb.settings()
+        s.setAttribute(ws.AutoLoadImages, bconf.AUTO_LOAD_IMAGES)
+        s.setAttribute(ws.JavascriptEnabled, bconf.JAVASCRIPT_ENABLED)
+        s.setAttribute(ws.JavascriptCanOpenWindows, bconf.JAVASCRIPT_CAN_OPEN_WINDOWS)
+        s.setAttribute(ws.LocalStorageEnabled, bconf.LOCAL_STORAGE_ENABLED)
+        s.setAttribute(ws.LocalContentCanAccessRemoteUrls, bconf.LOCAL_CONTENT_CAN_ACCESS_REMOTE_URLS)
+        s.setAttribute(ws.LocalContentCanAccessFileUrls, bconf.LOCAL_CONTENT_CAN_ACCESS_FILE_URLS)
+        s.setAttribute(ws.ErrorPageEnabled, bconf.ERROR_PAGES_ENABLED)
+        s.setAttribute(ws.PluginsEnabled, bconf.PLUGINS_ENABLED)
+        s.setAttribute(ws.WebGLEnabled, bconf.WEBGL_ENABLED)
+        s.setAttribute(ws.AllowRunningInsecureContent, bconf.ALLOW_RUNNING_INSECURE_CONTENT)
+        s.setAttribute(ws.AllowGeolocationOnInsecureOrigins, bconf.ALLOW_GEOLOCATION_ON_INSECURE_ORIGINS)
+        s.setAttribute(ws.ShowScrollBars, bconf.SHOW_SCROLL_BARS)
+        s.setAttribute(ws.DnsPrefetchEnabled, bconf.DNS_PREFETCH_ENABLED)
+
     def _get_page_source(self):
         if self.pwb.loadFinished:
             self.pwb.page().toHtml(self._page_to_var)
@@ -36,6 +52,7 @@ class PyWebBrowser(QMainWindow):
         self.page_source = html
         self.source_timer.stop()
         self._to_json()
+        self.accept()
 
     def _on_cookie(self, cookie):
         for i in self.raw_cookies:
@@ -55,14 +72,6 @@ class PyWebBrowser(QMainWindow):
                 "httpOnly": i.isHttpOnly()
             }
             self.cookie_list.append(data)
-        print("Cookies")
-        print(self.cookie_list)
-
-    def _init_settings(self):
-        self.pwb.settings().setAttribute(QWebEngineSettings.ScreenCaptureEnabled, True)
-        self.pwb.settings().setAttribute(QWebEngineSettings.LocalContentCanAccessRemoteUrls, True)
-        self.pwb.settings().setAttribute(QWebEngineSettings.JavascriptCanOpenWindows, False)
-        self.pwb.settings().setAttribute(QWebEngineSettings.ShowScrollBars, False)
 
     def get(self, url: str):
         self.req_obj.setUrl(QUrl().fromUserInput(url))
@@ -72,6 +81,3 @@ class PyWebBrowser(QMainWindow):
         self.source_timer.start()
 
         self.pwb.load(self.req_obj)
-
-        if bconf.DEBUG == 1:
-            self.pwb.show()
