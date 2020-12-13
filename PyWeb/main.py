1e2f8f72a33be4f3f973eff4f3638700e2071dd4
iiestIT <it.iiest.de@gmail.com>
2020-12-13 12:34:10 +0100

    fix blank window

--- PyWeb/main.py
+++ PyWeb/main.py
@@ -5,12 +5,13 @@ from PyWeb.utils.xvfb import VirtualDisplay
 
 
 def get(url: str):
-    vd = VirtualDisplay()
-    vd.init_xvfb()
+    # vd = VirtualDisplay()
+    # vd.init_xvfb()
     app = QApplication([])
     pw = PyWebBrowser()
     pw.get(url)
-    pw.setAttribute(Qt.WA_DeleteOnClose)
+    pw.setAttribute(Qt.WA_DeleteOnClose, True)
+    pw.setAttribute(Qt.WA_DontShowOnScreen, True)
     if pw.exec_() == QDialog.Accepted:
-        vd.stop_xvfb()
+        # vd.stop_xvfb()
         return pw.return_dict
