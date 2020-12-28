6c4d9f96aac8b60ede31d065d77bcfdd817b9851
iiestIT <it.iiest.de@gmail.com>
2020-12-28 21:03:29 +0100

    splitted browser.py in multiple parts

--- Py2Web/browser.py
+++ Py2Web/browser.py
@@ -1,15 +1,14 @@
 from PySide2.QtCore import QUrl, Qt
-from PySide2.QtWebEngineWidgets import QWebEngineView, QWebEngineProfile, QWebEnginePage
-from PySide2.QtWebEngineWidgets import QWebEngineSettings as ws
+from PySide2.QtWebEngineWidgets import QWebEngineView, QWebEnginePage
 from PySide2.QtWebEngineCore import QWebEngineHttpRequest
 from PySide2.QtWidgets import QDialog
 from PySide2.QtNetwork import QNetworkCookie
 from Py2Web.config import BaseConfig
-import random
+from Py2Web.engine import Py2WebProfile
 
 
 class Py2WebBrowser(QDialog):
-    def __init__(self, settings=None):
+    def __init__(self, settings=BaseConfig):
         super(Py2WebBrowser, self).__init__()
         self.setAttribute(Qt.WA_DeleteOnClose, True)
         self.setAttribute(Qt.WA_DontShowOnScreen, True)
@@ -17,18 +16,14 @@ class Py2WebBrowser(QDialog):
         self.pwb = QWebEngineView()
         self.pwb.setAttribute(Qt.WA_DeleteOnClose, True)
 
-        if settings is not None:
-            self.bconf = settings
-        else:
-            self.bconf = BaseConfig()
+        self.bconf = settings()
 
         self.raw_cookies = []
         self.cookie_list = []
 
         self.req_obj = QWebEngineHttpRequest()
 
-        profile = QWebEngineProfile("pyweb", self.pwb)
-        profile.setHttpUserAgent(random.choice(self.bconf.USER_AGENT_LIST))
+        profile = Py2WebProfile("pyweb", self.pwb, self.bconf)
         cookie_store = profile.cookieStore()
 
         cookie_store.cookieAdded.connect(self._on_cookie)
@@ -36,23 +31,7 @@ class Py2WebBrowser(QDialog):
         wp = QWebEnginePage(profile, self.pwb)
         self.pwb.setPage(wp)
 
-        self._settings()
         self.pwb.show()
-
-    def _settings(self):
-        self.pwb.settings().setAttribute(ws.AutoLoadImages, self.bconf.AUTO_LOAD_IMAGES)
-        self.pwb.settings().setAttribute(ws.JavascriptEnabled, self.bconf.JAVASCRIPT_ENABLED)
-        self.pwb.settings().setAttribute(ws.JavascriptCanOpenWindows, self.bconf.JAVASCRIPT_CAN_OPEN_WINDOWS)
-        self.pwb.settings().setAttribute(ws.LocalStorageEnabled, self.bconf.LOCAL_STORAGE_ENABLED)
-        self.pwb.settings().setAttribute(ws.LocalContentCanAccessRemoteUrls, self.bconf.LOCAL_CONTENT_CAN_ACCESS_REMOTE_URLS)
-        self.pwb.settings().setAttribute(ws.LocalContentCanAccessFileUrls, self.bconf.LOCAL_CONTENT_CAN_ACCESS_FILE_URLS)
-        self.pwb.settings().setAttribute(ws.ErrorPageEnabled, self.bconf.ERROR_PAGES_ENABLED)
-        self.pwb.settings().setAttribute(ws.PluginsEnabled, self.bconf.PLUGINS_ENABLED)
-        self.pwb.settings().setAttribute(ws.WebGLEnabled, self.bconf.WEBGL_ENABLED)
-        self.pwb.settings().setAttribute(ws.AllowRunningInsecureContent, self.bconf.ALLOW_RUNNING_INSECURE_CONTENT)
-        self.pwb.settings().setAttribute(ws.AllowGeolocationOnInsecureOrigins, self.bconf.ALLOW_GEOLOCATION_ON_INSECURE_ORIGINS)
-        self.pwb.settings().setAttribute(ws.ShowScrollBars, self.bconf.SHOW_SCROLL_BARS)
-        self.pwb.settings().setAttribute(ws.DnsPrefetchEnabled, self.bconf.DNS_PREFETCH_ENABLED)
 
     def _loadFinished(self):
         if len(self.js_script) > 0:
